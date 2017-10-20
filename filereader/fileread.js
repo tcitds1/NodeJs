4a039dfe6ba1be39a388e0295bb56345519536ed
konyahoshi <konyahoshi@qq.com>
2017-10-20 20:15:31 +0800

    添加源代码库

--- filereader/fileread.js
+++ filereader/fileread.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 fs.readdir(process.cwd(),function (err,files) {
+    var stats = [];// 记录stat对象
     console.log(" ");
     if(!files.length){
         return console.log(' \033[31m no files to show! \033[39m\n');
@@ -9,11 +10,12 @@ fs.readdir(process.cwd(),function (err,files) {
     function file(i) {
         var filename = files[i];
         fs.stat(__dirname+'/'+filename,function (err, stat) {
+            stats[i] = stat;
             if(stat.isDirectory()){
-                console.log('\033[36m '+ filename + '\033[39m\n');
+                console.log(i +'  '+ '\033[36m '+ filename + '\033[39m\n');
             }
             else {
-                console.log(' \033[90m'+ filename+ '\033[39m\n');
+                console.log(i +'  '+ ' \033[90m'+ filename+ '\033[39m\n');
             }
         })
         i++;
@@ -24,6 +26,25 @@ fs.readdir(process.cwd(),function (err,files) {
         }
         else{
             file(i);
+            read();
+        }
+    }
+    function read() {
+        process.stdin.setEncoding('utf8');
+        process.stdin.on("data",option)
+    }
+    function option(data) {
+        if(!files[Number(data)]){
+            process.stdout.write(' \033[33mEnter your choice: \033[39m\n');
+            process.stdin.resume();
+        }
+        else {
+            var filename = files[Number(data)];
+            process.stdin.pause();
+            fs.readFile(__dirname+"/"+filename,'utf-8',function (err, data) {
+                console.log(" ");
+                console.log('\033[90m' + data.replace(/(.*)/g, '          $1') +  '\033[39m');
+            });
         }
     }
     file(0);
